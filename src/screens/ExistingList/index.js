bf5552ae943b43bfefcd11a9202e91339c964b8d
Bryce Fadako <fadakob@chc.edu>
2022-10-12 10:28:42 -0400

    10/12/22

--- src/screens/ExistingList/index.js
+++ src/screens/ExistingList/index.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Text,TextInput,View,Pressable} from 'react-native';
+import {Text,TextInput,View,Pressable,Alert} from 'react-native';
 import styles from './styles';
 import { useNavigation } from '@react-navigation/native';
 
@@ -52,6 +52,38 @@ const ExistingListScreen = props => {
     
     }
     const onListDelete = () => {
+        return Alert.alert(
+            // title
+            'Confirm',
+            //message
+            'Are you sure you want to delete this list?',
+            //buttons
+            [
+                {
+                    text: 'Yes',
+                    onPress: () => {
+                        shopperDB.transaction(txn =>{
+                            txn.executeSql(
+                                `DELETE FROM ${listsTableName} WHERE id = ${post.id}`,
+                                [],
+                                () => {
+                                    console.log(`${name} deleted sucessfully`);
+                                },
+                                error => {
+                                    console.log('Error on deleting list' + error.message);
+                                }
+                            );
+                        });
+                        alert('List Deleted!');
+                        navigation.navigate('Start Shopping!');
+
+                    },
+                },
+                {
+                    text: 'No',
+                },
+            ],
+        );
        
     }
     const onAddItem = () => {
